4d2c0efc5b67976842eb6112473a4fc4775575e6
Micha Alt <micha@haus23.net>
2020-12-01 13:19:55 +0100

    Set up tailwind and clean up scaffolded template.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function FOHApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default FOHApp;
